e0bcad7cb8d263f3778b161d5f901575bdb88391
gaboUTN34 <gaandradeb@utn.edu.ec>
2025-10-05 09:28:58 -0500

    Creación del script principal del dashboard para posteriormente implementar los datos del equipo.
    Signed-off-by: gaboUTN34 <gaandradeb@utn.edu.ec>

---
+++ EC04/VISUALIZACION DE INFRAESTRUCTURA DE REDES PUBLICAS EN EL DISTRITO METROPOLITANO DE QUITO/dashboard/vis_dashboard.py
@@ -0,0 +1,52 @@
+# Este script es para ejecutar el dashboard y presentar los datos obtenidos anteriormente.
+# Se ejecuta mediante el módulo dash y el módulo flask (en pruebas).
+
+# TODO 1: implementar el dashboard en el proyecto como tal.
+# TODO 2: intentar que el dashboard sea interactivo.
+# TODO 3: presentar los datos de forma ordenada y legible.
+# TODO 4: completar las funciones declaradas para integrarlas con los demás módulos del equipo.
+
+# Módulos principales de este script.
+import dash
+from dash import html
+
+# TODO: Módulos realizados por el equipo, para la presentación de los datos (completar).
+
+
+# TODO: Cargar y procesar datos
+
+# Inicializar la app Dash
+app = dash.Dash(__name__)
+
+# Diseño del dashboard
+app.layout = html.Div([
+    html.H1("Dashboard del proyecto - Puntos WiFi del Distrito Metropolitano de Quito", style={'textAlign': 'center'}),
+
+    # Sección: Gráficos estadísticos
+    html.Div([
+        html.H2("Estadísticas de cobertura"),
+        html.Iframe(
+
+        )
+    ]),
+
+    # Sección: Mapa interactivo
+    html.Div([
+        html.H2("Mapa de Puntos WiFi"),
+        html.Iframe(
+
+        )
+    ]),
+
+    # Sección: Zonas Prioritarias
+    html.Div([
+        html.H2("Zonas Prioritarias para Expansión"),
+        html.Iframe(
+
+        )
+    ]),
+])
+
+# TODO: al finalizar el código cambiar la condición a False.
+if __name__ == '__main__':
+    app.run(debug=True)
